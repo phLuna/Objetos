3a9ae14a82d0544762c55b171c5ab73498a61ce1
Luna <138384681+phLuna@users.noreply.github.com>
2023-12-06 19:46:37 -0300

    Sistema para 'upar' faixas pronto.
    Ainda não adicionado ao CRUD.

--- alunos.py
+++ alunos.py
@@ -15,3 +15,23 @@ class Aluno:
     
     def subir_faixa(self):
         Aluno.faixa = input('Insira a nova faixa: ')
+        if Aluno.faixa == 'Branca':
+            Aluno.Faixa == 'Cinza'
+        elif Aluno.faixa == 'Cinza':
+            Aluno.Faixa == 'Amarela'
+        elif Aluno.faixa == 'Amarela':
+            Aluno.Faixa == 'Laranja'
+        elif Aluno.faixa == 'Laranja':
+            Aluno.Faixa == 'Verde'
+        elif Aluno.faixa == 'Verde':
+            Aluno.Faixa == 'Azul'
+        elif Aluno.faixa == 'Azul':
+            Aluno.Faixa == 'Roxa'
+        elif Aluno.faixa == 'Roxa':
+            Aluno.Faixa == 'Marrom'
+        elif Aluno.faixa == 'Marrom':
+            Aluno.Faixa == 'Preta'
+        elif Aluno.faixa == 'Preta':
+            Aluno.Faixa == 'Coral'
+        elif Aluno.faixa == 'Coral':
+            Aluno.Faixa == 'Vermelha'
